9709844740bb9c6e604a70d753a40e2840c0e07e
TWM <15152806231@163.com>
2019-09-02 15:54:34 +0800

    字符流：使用字符流读取文件

---
+++ src/com/gtmap/io/test/Test_IO_11.java
@@ -0,0 +1,25 @@
+package com.gtmap.io.test;
+
+import java.io.File;
+import java.io.FileReader;
+
+public class Test_IO_11 {
+	public static void main(String[] args) {
+		//准备文件dnf.txt其中的内容是XY
+		File file = new File("d:/dnf.txt");
+		//创建基于文件的Reader
+		try(FileReader fr = new FileReader(file)) {
+			//创建字符数组，其长度就是文件长度
+			char[] all = new char[(int) file.length()];
+			//以字符流的形式读取文件所有内容
+			fr.read(all);
+			for (char c : all) {
+				//打印出来是 X Y
+				System.out.println(c);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+}
