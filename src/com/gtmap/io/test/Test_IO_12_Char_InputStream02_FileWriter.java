ffa4d8457cc0f0e3450be6b3fe6b7c3b25021b89
TWM <15152806231@163.com>
2019-09-02 17:08:59 +0800

    使用字符流把字符串写入到文件：FileWriter 是Writer的子类，以FileWriter 为例把字符串写入到文件

--- src/com/gtmap/io/test/Test_IO_12_Char_InputStream02_FileWriter.java
+++ src/com/gtmap/io/test/Test_IO_12_Char_InputStream02_FileWriter.java
@@ -2,7 +2,12 @@ package com.gtmap.io.test;
 
 import java.io.File;
 import java.io.FileWriter;
-
+/**
+ * 使用字符流把字符串写入到文件：
+ * FileWriter 是Writer的子类，以FileWriter 为例把字符串写入到文件
+ * @author Administrator
+ *
+ */
 public class Test_IO_12_Char_InputStream02_FileWriter {
 	public static void main(String[] args) {
 		//准备文件lol2.txt
