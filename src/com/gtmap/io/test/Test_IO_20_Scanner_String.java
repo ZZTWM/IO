5d966b2c3aa9cfa6a1daf5b97280fcbc64d8eb1a
TWM <15152806231@163.com>
2019-09-03 09:57:23 +0800

    使用Scanner读取字符串

---
+++ src/com/gtmap/io/test/Test_IO_20_Scanner_String.java
@@ -0,0 +1,19 @@
+package com.gtmap.io.test;
+
+import java.util.Scanner;
+
+/**
+ * 	使用System.in.read虽然可以读取数据，但是很不方便
+ * 	使用Scanner就可以逐行读取了
+ * @author Administrator
+ *
+ */
+public class Test_IO_20_Scanner_String {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		while(true){
+			String line = scan.nextLine();
+			System.out.println(line);
+		}
+	}
+}
