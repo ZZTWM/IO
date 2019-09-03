f49f81395a097753d3d1c6ff524ec3484b10e5f6
TWM <15152806231@163.com>
2019-09-03 09:45:01 +0800

    Hero类

--- src/com/charactor/Hero.java
+++ src/com/charactor/Hero.java
@@ -3,7 +3,6 @@ package com.charactor;
 import java.io.Serializable;
 
 public class Hero implements Serializable{
-
 	/**
 	 * 表示这个类当前的版本，如果有了变化，比如新设计了属性，就应该修改这个版本号
 	 */
@@ -11,5 +10,14 @@ public class Hero implements Serializable{
 	
 	public String name;
 	public float hp;
+	
+	public Hero(){
+		
+	}
+
+	public Hero(String name) {
+		super();
+		this.name = name;
+	}
 	
 }
